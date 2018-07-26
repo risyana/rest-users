3c8a245a84020ed11f02d258bad6426bbd0ee9e7
Eka Risyana <risyana@gmail.com>
2018-07-26 14:17:52 +0700

    implement CRUD for each routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,32 +1,105 @@
 const express = require('express');
+const db = require('../../database');
 
 const router = express.Router();
+let stmt = '';
 
+// GET ALL USERS
 router.get('/', (req, res) => {
-  res.status(200).json({ message: 'GET all users' });
-});
+  const rows = [];
+  stmt = 'SELECT * FROM USERS';
 
-router.post('/', (req, res) => {
-  const newUser = { ...req.body };
-  res.status(201).json({
-    message: 'POST new user',
-    newUser,
+  db.each(stmt, (err, row) => {
+    if (err) {
+      res.status(404).json({ message: err.message });
+    } else {
+      rows.push(row);
+    }
+  }, () => {
+    res.status(200).json({
+      message: 'GET all users',
+      user: rows,
+    });
   });
 });
 
+// INSERT NEW USER
+router.post('/', (req, res) => {
+  const newUser = { ...req.body };
+  stmt = 'INSERT INTO USERS VALUES (?, ?, ?, ?, ?)';
+
+  const newUserArray = [null, newUser.email, newUser.name, newUser.password, newUser.phone];
+  db.run(stmt, newUserArray, (err) => {
+    if (err) {
+      res.status(404).json({ message: err.message });
+    } else {
+      res.status(201).json({
+        message: 'POST new user',
+        newUser,
+      });
+    }
+  });
+});
+
+// GET ONE SPECIFIC USER
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  res.status(200).json({ message: `GET specific user ${id}` });
+  const rows = [];
+  stmt = 'SELECT * FROM USERS WHERE id = ?';
+
+  db.each(stmt, id, (err, row) => {
+    if (err) {
+      res.status(404).json({ message: err.message });
+    } else {
+      rows.push(row);
+    }
+  }, () => {
+    if (rows.length < 1) {
+      res.status(200).json({
+        message: `GET user ${id}`,
+        user: null,
+      });
+    } else {
+      res.status(200).json({
+        message: `GET user ${id}`,
+        user: rows[0],
+      });
+    }
+  });
 });
 
+// UPDATE SPECIFIC USER
 router.patch('/:id', (req, res) => {
+  const updatedUser = { ...req.body };
   const { id } = req.params;
-  res.status(200).json({ message: `PATCH specific user ${id}` });
+  const updatedUserArray = [updatedUser.email, updatedUser.name,
+    updatedUser.password, updatedUser.phone, id];
+  stmt = 'UPDATE USERS SET email = ?, name = ?, password = ?, phone = ? WHERE id = ?';
+
+  db.run(stmt, updatedUserArray, (err) => {
+    if (err) {
+      res.status(404).json({ message: err.message });
+    } else {
+      res.status(201).json({
+        message: `PATCH specific user ${id}`,
+        updatedUser,
+      });
+    }
+  });
 });
 
+// DELETE SPECIFIC USER
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
-  res.status(200).json({ message: `DELETE specific user ${id}` });
+  stmt = 'DELETE FROM USERS WHERE id = (?)';
+
+  db.run(stmt, id, (err) => {
+    if (err) {
+      res.status(404).json({ message: err.message });
+    } else {
+      res.status(200).json({ message: `DELETE specific user ${id}` });
+    }
+  });
 });
 
 module.exports = router;
