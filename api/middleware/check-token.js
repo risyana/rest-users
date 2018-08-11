7eeb23bb80b5a608b3d3dfaa5b3c89ede8b3bbe5
Eka Risyana <risyana@gmail.com>
2018-08-12 05:58:36 +0700

    no need to specify status. It will be defined in app.js

--- api/middleware/check-token.js
+++ api/middleware/check-token.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   if (!req.headers.authorization) {
     const error = new Error('Auth failed : no auth header');
-    error.status = 401;
     next(error);
   } else {
     const token = req.headers.authorization.split(' ')[1];
