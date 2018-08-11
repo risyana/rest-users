1abf2e976a001f9a16cf40732e59c6adc4cb30c2
Eka Risyana <risyana@gmail.com>
2018-08-12 05:45:33 +0700

    for the sake of testability wise, create error object to be passed instead of return it directly.

--- api/middleware/check-token.js
+++ api/middleware/check-token.js
@@ -2,19 +2,19 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   if (!req.headers.authorization) {
-    res.status(401).json({
-      message: 'Auth Failed',
-    });
-  }
-
-  const token = req.headers.authorization.split(' ')[1];
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_KEY || 'rahasia');
-    req.users = decoded;
-    next();
-  } catch (err) {
-    res.status(401).json({
-      message: `Auth failed ${err}`,
-    });
+    const error = new Error('Auth failed : no auth header');
+    error.status = 401;
+    next(error);
+  } else {
+    const token = req.headers.authorization.split(' ')[1];
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_KEY || 'rahasia');
+      req.users = decoded;
+      next();
+    } catch (err) {
+      res.status(401).json({
+        message: `Auth failed ${err}`,
+      });
+    }
   }
 }
