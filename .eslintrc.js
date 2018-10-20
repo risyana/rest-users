9079918424fe9542715dc30e30a5572656e21d63
Eka Risyana <risyana@gmail.com>
2018-10-20 08:05:57 +0700

    add some globals

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
     "extends": "airbnb-base",
+    "globals" : {
+        "describe" : true,
+        "it" : true,
+        "after" : true,
+        "before" : true
+    },
     "rules" : {
         "linebreak-style" : 0,
     }
